e95934a335041d2377b0a4bbf07cb86177a02533
Mugiwara <37186888+ciss84@users.noreply.github.com>
2020-05-26 20:33:27 +0200

    Update Patches505.cpp

--- src/Boot/Patches/Patches505.cpp
+++ src/Boot/Patches/Patches505.cpp
@@ -178,6 +178,17 @@ void Mira::Boot::Patches::install_prerunPatches_505()
 	kmem[0] = 0xEB;
 	kmem[1] = 0x38;
 	
+        // allow sys_dynlib_dlsym in all processes
+	kmem = (uint8_t *)&gKernelBase[0x00237F3A];
+	kmem[0] = 0xE9;
+	kmem[1] = 0xC1;
+	kmem[2] = 0x01;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+	kmem[5] = 0x90;
+	kmem[6] = 0x48;
+	kmem[7] = 0x8B;
+	
 	// allow sys_dynlib_dlsym in all processes
 	kmem = (uint8_t *)&gKernelBase[0x00237F3A];
 	kmem[0] = 0xE9;
