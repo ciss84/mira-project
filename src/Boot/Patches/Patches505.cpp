00ec5b594208c8e9957370c6c8bbe2ddb0b26cbb
Mugiwara <37186888+ciss84@users.noreply.github.com>
2020-05-27 03:00:26 +0200

    Add files via upload

--- src/Boot/Patches/Patches505.cpp
+++ src/Boot/Patches/Patches505.cpp
@@ -189,17 +189,6 @@ void Mira::Boot::Patches::install_prerunPatches_505()
 	kmem[6] = 0x48;
 	kmem[7] = 0x8B;
 	
-	// allow sys_dynlib_dlsym in all processes
-	kmem = (uint8_t *)&gKernelBase[0x00237F3A];
-	kmem[0] = 0xE9;
-	kmem[1] = 0xC1;
-	kmem[2] = 0x01;
-	kmem[3] = 0x00;
-	kmem[4] = 0x00;
-	kmem[5] = 0x90;
-	kmem[6] = 0x48;
-	kmem[7] = 0x8B;
-	
 	// flatz allow mangled symbol in dynlib_do_dlsym
 	kmem = (uint8_t *)&gKernelBase[0x002AFB47];
   kmem[0] = 0x90;
