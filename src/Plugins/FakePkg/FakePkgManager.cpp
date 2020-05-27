cda545cb82456fe4410f7a81e3fe7c5cf5dde0f6
Mugiwara <37186888+ciss84@users.noreply.github.com>
2020-05-27 20:01:58 +0200

    Add files via upload

--- src/Plugins/FakePkg/FakePkgManager.cpp
+++ src/Plugins/FakePkg/FakePkgManager.cpp
@@ -278,6 +278,14 @@ bool FakePkgManager::ShellCorePatch()
         WriteLog(LL_Error, "ssc_enable_data_mount_patch");
         return false;
     }
+    
+     s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssu_enable_debug_trophies_patch), 5, (void*)"\x31\xC0\x90\x90\x90", nullptr, true);
+    if (s_Ret < 0)
+    {
+        WriteLog(LL_Error, "ssu_enable_debug_trophies_patch ");
+        return false;
+    }
+    
     /*Utilities::PtraceIO(s_Process->p_pid, PIOD_WRITE_I, (void*)(s_TextStart + SHELLCORE_ENABLE_DEBUG_PKG_PATCH_1_1_OFFSET), xor__eax_eax, sizeof(xor__eax_eax));
     Utilities::PtraceIO(s_Process->p_pid, PIOD_WRITE_I, (void*)(s_TextStart + SHELLCORE_ENABLE_DEBUG_PKG_PATCH_1_2_OFFSET), xor__eax_eax, sizeof(xor__eax_eax));
     Utilities::PtraceIO(s_Process->p_pid, PIOD_WRITE_I, (void*)(s_TextStart + SHELLCORE_ENABLE_DEBUG_PKG_PATCH_1_3_OFFSET), xor__eax_eax, sizeof(xor__eax_eax));
