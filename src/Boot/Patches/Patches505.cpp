c43e5ac8387bc0be585d3b0406fae95172228026
Mugiwara <37186888+ciss84@users.noreply.github.com>
2020-05-27 14:33:11 +0200

    Update Patches505.cpp

--- src/Boot/Patches/Patches505.cpp
+++ src/Boot/Patches/Patches505.cpp
@@ -231,7 +231,7 @@ void Mira::Boot::Patches::install_prerunPatches_505()
 	kmem[2] = 0x00;
 	kmem[3] = 0x00;
 	
-  // Firmware spoof 7.50
+  // Firmware spoof 7.51
   kmem = (uint8_t *)&gKernelBase[0x014A63F0];
   kmem[0] = 0x00;
   kmem[1] = 0x00;
