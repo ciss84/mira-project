0be52b2be5997f16f904f2fe2fe94bd00f828459
Mugiwara <37186888+ciss84@users.noreply.github.com>
2020-05-29 13:14:50 +0200

    Add files via upload

--- src/Plugins/FakePkg/FakePkgManager.cpp
+++ src/Plugins/FakePkg/FakePkgManager.cpp
@@ -272,24 +272,24 @@ bool FakePkgManager::ShellCorePatch()
         return false;
     }
     
-    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssc_enable_data_mount_patch), 5, (void*)"\x31\xC0\xFF\xC0\x90", nullptr, true);
+    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssc_remote_pkg_patch), 5, (void*)"\x31\xC0\xFF\xC0\x90", nullptr, true);
     if (s_Ret < 0)
     {
-        WriteLog(LL_Error, "ssc_enable_data_mount_patch");
+        WriteLog(LL_Error, "ssc_remote_pkg_patch");
         return false;
     }
 
-    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssc_make_pkgs_installer_working_with_external_hdd), 1, (void*)"\0", nullptr, true);
+    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssc_make_pkgs_installer_working_with_external_hdd_patch), 1, (void*)"\0", nullptr, true);
     if (s_Ret < 0)
       {
-        WriteLog(LL_Error, "ssc_make_pkgs_installer_working_with_external_hdd ");
+        WriteLog(LL_Error, "ssc_make_pkgs_installer_working_with_external_hdd_patch ");
         return false;
     }
     
-    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssc_enable_support_external_hdd), 1, (void*)"\xEB", nullptr, true);
+    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + ssc_enable_support_external_hdd_patch), 1, (void*)"\xEB", nullptr, true);
     if (s_Ret < 0)
       {
-        WriteLog(LL_Error, "ssc_enable_support_external_hdd");
+        WriteLog(LL_Error, "ssc_enable_support_external_hdd_patch");
         return false;
     }
     
