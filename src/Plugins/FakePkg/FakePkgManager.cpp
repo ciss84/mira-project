5cc1518e50e0855ae1690a740d15a43612019980
ciss84 <cj84_84120@outlook.com>
2020-06-08 00:34:26 +0200

    add_patch_downgrade

--- src/Plugins/FakePkg/FakePkgManager.cpp
+++ src/Plugins/FakePkg/FakePkgManager.cpp
@@ -359,13 +359,6 @@ bool FakePkgManager::ShellCorePatch()
         return false;
     }
     
-    s_Ret = Utilities::ProcessReadWriteMemory(s_Process, (void*)(s_TextStart + kdlsym_addr_sceRegMgrGetInt), 1, (void*)  "\1", nullptr, true);
-    if (s_Ret < 0)
-      {
-        WriteLog(LL_Error, "kdlsym_addr_sceRegMgrGetInt");
-        return false;
-    }
-    
     /*Utilities::PtraceIO(s_Process->p_pid, PIOD_WRITE_I, (void*)(s_TextStart + SHELLCORE_ENABLE_DEBUG_PKG_PATCH_1_1_OFFSET), xor__eax_eax, sizeof(xor__eax_eax));
     Utilities::PtraceIO(s_Process->p_pid, PIOD_WRITE_I, (void*)(s_TextStart + SHELLCORE_ENABLE_DEBUG_PKG_PATCH_1_2_OFFSET), xor__eax_eax, sizeof(xor__eax_eax));
     Utilities::PtraceIO(s_Process->p_pid, PIOD_WRITE_I, (void*)(s_TextStart + SHELLCORE_ENABLE_DEBUG_PKG_PATCH_1_3_OFFSET), xor__eax_eax, sizeof(xor__eax_eax));
