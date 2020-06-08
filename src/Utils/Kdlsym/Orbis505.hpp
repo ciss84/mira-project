1c8724cdebb419374b4a17d78fece87702dec1e5
ciss84 <cj84_84120@outlook.com>
2020-06-08 02:01:50 +0200

    Patch debug setting errors TESTKIT

--- src/Utils/Kdlsym/Orbis505.hpp
+++ src/Utils/Kdlsym/Orbis505.hpp
@@ -205,19 +205,17 @@ for the platforms that do enable kernel ASLR (Address Space Layout Randomization
 #define ssc_sceKernelIsGenuineCEX_patchC                    0x007E5A13
 #define ssc_sceKernelIsGenuineCEX_patchD                    0x0094715B
 #define ssc_sceKernelIsGenuineCEX_patchE                    0x006ABE39
-
-// SceShellCore patches - call RemotePackages
 #define ssc_sceKernelIsGenuineCEX_patchF                    0x00319A53
 
 // enable savedata mounting
-#define ssc_sceKernelIsGenuineCEX_patchG                    0x00D42843
-#define ssc_sceKernelIsGenuineCEX_patchH                    0x007E4DC0
-#define ssc_sceKernelIsGenuineCEX_patchI                    0x00068BA0
-#define ssc_sceKernelIsGenuineCEX_patchJ                    0x0006A349
-#define ssc_sceKernelIsGenuineCEX_patchK                    0x000686AE
-#define ssc_sceKernelIsGenuineCEX_patchL                    0x00067FCA
-#define ssc_sceKernelIsGenuineCEX_patchM                    0x00067798
-#define ssc_sceKernelIsGenuineCEX_patchN                    0x000679D5
+#define ssc_savedata_patchA                    0x00D42843
+#define ssc_savedata_patchB                    0x007E4DC0
+#define ssc_savedata_patchC                    0x00068BA0
+#define ssc_savedata_patchD                    0x0006A349
+#define ssc_savedata_patchE                    0x000686AE
+#define ssc_savedata_patchF                    0x00067FCA
+#define ssc_savedata_patchG                    0x00067798
+#define ssc_savedata_patchH                    0x000679D5
 
 // SceShellCore patches - call nidf_libSceDipsw
 #define ssc_nidf_libSceDipsw_patchA                         0x0016D087
